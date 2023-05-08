94af2ae16c92be62f275cfba063c1b8939b24e01
Braden Kinard <braden.kinard@artifact.io>
2023-05-08 17:26:25 -0400

    add pytest to requirements

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ REQUIRES = [
     "certifi >= 14.5.14",
     "frozendict ~= 2.3.4",
     "python-dateutil ~= 2.7.0",
+    "pytest ~= 7.3.1",
     "setuptools >= 21.0.0",
     "typing_extensions ~= 4.3.0",
     "urllib3 ~= 1.26.7",
